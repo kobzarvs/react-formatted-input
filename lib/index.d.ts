520bf0eaaf27a90fc686665f5b606d09fa66ea9c
Valeriy Kobzar <kobzarvs@gmail.com>
2021-11-02 20:06:34 +0300

    refactoring

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,15 @@
-export * from './components/formatted-input';
+/// <reference types="react" />
+export interface FormattedInputProps {
+    label: string;
+    value: string;
+    mask: string;
+    placeholder: string;
+    errorMessage: string;
+    parser?: (str: string | null | undefined) => string;
+    formatter?: (str: string, mask: string) => string;
+    onChange: (str: string) => void;
+}
+export declare const defaultParser: (value: string | null | undefined) => string;
+export declare const defaultFormatter: (value: string, mask: string) => string;
+export declare const Index: ({ label, value, parser, mask, placeholder, errorMessage, formatter, onChange, }: FormattedInputProps) => JSX.Element;
 //# sourceMappingURL=index.d.ts.map
--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-fmt-input' {
-  export interface FormattedInputProps;
-  export const FormattedInput = (FormattedInputProps) => React.ReactNode;
-  export const defaultFormatter = (value: string, mask: string) => string;
-  export const defaultParser = (value: string | null | undefined) => string;
-}
